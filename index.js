977dc77377f41d4dae0f43b1ef663642df90d927
Soul-Remix <soulremix-webdev@pm.me>
2021-09-09 20:23:53 +0300

    Create debounce fn

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 const chokidar = require('chokidar');
 const prog = require('caporal');
 
+function debounce(fn, wait) {
+  let timeout;
+  return (...params) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fn(params);
+    }, wait);
+  };
+}
+
+const start = debounce(() => {
+  console.log('starting');
+}, 100);
+
 const path = process.argv[2] || '.';
 
 chokidar
   .watch(path)
-  .on('add', console.log('started'))
+  .on('add', start)
   .on('change', () => console.log('file changed'))
   .on('unlink', () => console.log('file unlinked'));
