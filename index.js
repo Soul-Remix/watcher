258238b04a64f4ee33039a4157c6b9728e944e65
Soul-Remix <soulremix-webdev@pm.me>
2021-09-09 20:20:05 +0300

    add chokidar & caporal

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const chokidar = require('chokidar');
+const prog = require('caporal');
 
-console.log('i was called');
+const path = process.argv[2] || '.';
+
+chokidar
+  .watch(path)
+  .on('add', console.log('started'))
+  .on('change', () => console.log('file changed'))
+  .on('unlink', () => console.log('file unlinked'));
